5edf7fa18df2895a930e587be2dffdbf5a026dec
SebastianJL <sebastianjuncos@gmail.com>
2022-03-30 19:40:34 -0600

    Fixed emoji position

--- save-recordings/src/App.js
+++ save-recordings/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World 🥴✌️
+          Hello World ✌️🥴
         </p>
       </header>
     </div>
