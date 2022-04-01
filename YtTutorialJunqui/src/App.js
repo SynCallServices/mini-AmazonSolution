ca0bf50a9147c8ca392edd8e4c512abae34df52e
SebastianJL <sebastianjuncos@gmail.com>
2022-04-01 17:13:57 -0600

    Finished login tutorial (kinda), and page is ready (again... kinda

--- YtTutorialJunqui/src/App.js
+++ YtTutorialJunqui/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// Shit's not working as it should 🥴, but login's not our problem 🤷‍♀️
+// This website might help you https://ui.docs.amplify.aws/components/authenticator
 
-function App() {
+import './App.css';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+// Install npm install --save aws-amplify @aws-amplify/ui-react
+// Imports needed only for login (probs, idk ¯\_ಠ_ಠ_/¯)
+import { withAuthenticator } from '@aws-amplify/ui-react';
+// import '@aws-amplify/ui-react/style.css';
+
+Amplify.configure(awsconfig);
+
+function App({ isPassedToWithAuthenticator, signOut, user }) {
+  if (!isPassedToWithAuthenticator) {
+    throw new Error(`isPassedToWithAuthenticator was not provided`);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello {user.username}</h1>
+        <button onClick={signOut}>Sign out</button>
       </header>
     </div>
   );
 }
 
-export default App;
+// Exporting the app within the withAuthenticator will enable Amazon cognito user auth
+export default withAuthenticator(App);
+
+export async function getStaticProps() {
+  return {
+    props: {
+      isPassedToWithAuthenticator: true,
+    },
+  };
+}
