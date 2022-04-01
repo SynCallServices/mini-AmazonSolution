b9467f7ee654e13a6006847c0772d276a8fdcec3
caroortegab <57368415+caroortegab@users.noreply.github.com>
2022-04-01 12:37:37 -0600

    Hello_world-caro y mati

---
+++ hello-world/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          HELLO <code> WORLD!!!! :)</code> CARO Y MATI
+        </p>
+        <a
+          className="TIK TOK"
+          href="https://vm.tiktok.com/ZML9pvm3K/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          TIK TOK
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
