56d2ecc6ec026419db0ccde90c36121959b2eac1
SebastianJL <sebastianjuncos@gmail.com>
2022-04-01 18:54:13 -0600

    Login works (still needs enchulada for the UI tho)

--- YtTutorialJunqui/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ YtTutorialJunqui/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "yttutorialjunquidc7f9ffa": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- YtTutorialJunqui/src/App.js
+++ YtTutorialJunqui/src/App.js
@@ -1,5 +1,6 @@
 // Shit's not working as it should 🥴, but login's not our problem 🤷‍♀️
 // This website might help you https://ui.docs.amplify.aws/components/authenticator
+// But this one's even better https://docs.amplify.aws/lib/auth/getting-started/q/platform/js/#create-authentication-service 🙂
 
 import './App.css';
 import { Amplify } from 'aws-amplify';
@@ -7,15 +8,12 @@ import awsconfig from './aws-exports';
 // Install npm install --save aws-amplify @aws-amplify/ui-react
 // Imports needed only for login (probs, idk ¯\_ಠ_ಠ_/¯)
 import { withAuthenticator } from '@aws-amplify/ui-react';
-// import '@aws-amplify/ui-react/style.css';
+
+// import '../node_modules/@aws-amplify/ui-react/dist/styles.css';
 
 Amplify.configure(awsconfig);
 
-function App({ isPassedToWithAuthenticator, signOut, user }) {
-  if (!isPassedToWithAuthenticator) {
-    throw new Error(`isPassedToWithAuthenticator was not provided`);
-  }
-
+function App({ signOut, user }) {
   return (
     <div className="App">
       <header className="App-header">
@@ -29,10 +27,10 @@ function App({ isPassedToWithAuthenticator, signOut, user }) {
 // Exporting the app within the withAuthenticator will enable Amazon cognito user auth
 export default withAuthenticator(App);
 
-export async function getStaticProps() {
-  return {
-    props: {
-      isPassedToWithAuthenticator: true,
-    },
-  };
-}
+// export async function getStaticProps() {
+//   return {
+//     props: {
+//       isPassedToWithAuthenticator: true,
+//     },
+//   };
+// }
