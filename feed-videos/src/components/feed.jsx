9c5b541e9bb3e000dcacdfab3ee43af6eb2d0656
Matt <a01422885@itesm.mx>
2022-04-04 17:31:59 -0500

    front of the feed videos app

---
+++ feed-videos/src/components/feed.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Feed extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div></div>
+        );
+    }
+}
+ 
+export default Feed;
