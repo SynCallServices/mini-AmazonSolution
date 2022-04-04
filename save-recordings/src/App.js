a5eca3aaa221491453268a4d8d29164fb4d79fe2
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2022-04-04 17:52:06 -0500

    Added amplify auth packages to dependencies

--- save-recordings/src/App.js
+++ save-recordings/src/App.js
@@ -1,10 +1,16 @@
 import logo from './fotish.jpg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports'
+import {AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react'
+
+Amplify.configure(awsconfig)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <AmplifySignOut />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <br />Hello World ✌️🥴
@@ -14,4 +20,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
