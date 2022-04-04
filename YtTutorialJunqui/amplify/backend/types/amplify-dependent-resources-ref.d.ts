3eda052c347858104885a7eb958bdf98233e5e9f
SebastianJL <sebastianjuncos@gmail.com>
2022-04-04 17:16:14 -0500

    Added graphQl schema from tutorial

--- YtTutorialJunqui/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ YtTutorialJunqui/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,12 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "testAPI": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
     }
 }
