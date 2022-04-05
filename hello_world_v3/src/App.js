768ce2f552781f38fd2f817ada419e2ba517fc69
Miguel Medina R <A01023656@itesm.mx>
2022-04-05 00:00:32 -0500

    Added final hello world app using npx

---
+++ hello_world_v3/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="tiger.png" className="App-logo" alt="logo" />
+        <br></br>
+        <p>
+          Hello World & Happy Tiger's year!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/The-Hacker-Mike"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Check out my Github!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
