aadca0280cd59a58a960770abb426e5ba13db14e
Yibizo <diegomejiasuarez@gmail.com>
2022-04-07 18:06:57 -0500

    feature/login & register form with cognito

--- log-in/src/App.js
+++ log-in/src/App.js
@@ -2,53 +2,105 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import AWS from "aws-sdk";
-import fs from "fs";
 
-import 'cross-fetch/polyfill';
-import AmazonCognitoIdentity from 'amazon-cognito-identity-js';
+const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
 
-require("dotenv").config();
+AWS.config.update({
+    apiVersion: "latest",
+    region: process.env.REACT_APP_REGION,
+    accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY
+});
 
-AWS.config.update({ region: "us-east-1"});
-AWS.config.update("./config.json");
+const cognito = new AWS.CognitoIdentityServiceProvider({
+  region: process.env.REACT_APP_REGION
+});
 
-// AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-//   IdentityPoolId: "",
-//   Logins: {
-//     "www.amazon.com": "AMAZONTOKEN"
-//   }
-// })
+const CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
 
-var params = {
-  InstanceId: "",
-  Origin: ""
-};
+const poolData = {
+  UserPoolId: process.env.REACT_APP_USER_POOL_ID,
+  ClientId: process.env.REACT_APP_CLIENT_ID
+}
 
-// AWS.config.getCredentials(function(err) {
-//   console.log("Loading credentials...")
-//   if (err) console.log(err.stack);
-//   // credentials not loaded
-//   else {
-//     console.log("Access key:", AWS.config.credentials.accessKeyId);
-//   }
-// });
+const poolRegion = process.env.REACT_APP_REGION;
 
-const connect = new AWS.Connect({apiVersion: '2017-08-08'});
-connect.associateApprovedOrigin(params, function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-})
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+function RegisterUser(name, email, password){
+  let attributeList = [];
+  attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"name",Value:name}));
+  attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"email",Value:email}));
+
+  userPool.signUp(name, password, attributeList, null, function(err, result){
+      if (err) {
+          console.log(err);
+          return;
+      }
+      cognito.adminSetUserPassword({
+        UserPoolId: process.env.REACT_APP_USER_POOL_ID,
+        Username: name,
+        Password: password,
+        Permanent: true
+       }, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("success!!");
+        }
+      })
+  });
+}
+
+// RegisterUser()
+
+function LoginUser(user, password) {
+  var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+      Username : user,
+      Password : password,
+  });
+
+  console.log(authenticationDetails)
+
+  var userData = {
+      Username : user,
+      Pool : userPool
+  };
+  var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+  cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: function (result) {
+          console.log("entered success");
+      },
+      onFailure: function(err) {
+          console.log(err);
+      },
+      mfaRequired: function() {
+        console.log("code required");
+      },
+      newPasswordRequired: function() {
+        console.log("password required");
+        cognito.adminSetUserPassword({
+          UserPoolId: process.env.REACT_APP_USER_POOL_ID,
+          Username: user,
+          Password: password,
+          Permanent: true
+         }, function(err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("success!!");
+          }
+        })
+      }
+  });
+}
+
+// Login();
 
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {value: ""};
-
-    // this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
@@ -64,6 +116,7 @@ class LoginForm extends React.Component {
     password = password.value;
     console.log(name, password);
     event.preventDefault();
+    LoginUser(name, password);
     document.getElementById("loginForm").reset();
   }
 
@@ -86,24 +139,59 @@ class LoginForm extends React.Component {
   }
 }
 
+class RegisterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ""};
+  }
+
+  handleChange(event) {
+    console.log(event);
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert("Credentials entered");
+    console.log(document.forms[0]);
+    let { name, email, password } = document.forms[1];
+    name = name.value;
+    email = email.value;
+    password = password.value;
+    console.log(name, email, password);
+    event.preventDefault();
+    RegisterUser(name, email, password);
+    document.getElementById("registerForm").reset();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} id="registerForm">
+        <label>
+          Name:<br/>
+          <input name="name" type="text" required />
+        </label>
+        <br/>
+        <label>
+          Email:<br/>
+          <input name="email" type="text" required />
+        </label>
+        <br/>
+        <label>
+          Password:<br/>
+          <input name="password" type="password" required />
+        </label>
+        <br/>
+        <input type={"submit"}></input>
+      </form>
+    )
+  }
+}
+
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <LoginForm />
+      <RegisterForm />
     </div>
   );
 }
