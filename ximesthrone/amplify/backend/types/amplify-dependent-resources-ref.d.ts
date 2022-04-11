db33cb0097e82bdff5189691f9bb783099da479e
Ximena Gonzalez <ximena.glez@gmail.com>
2022-04-11 09:27:18 -0500

    intento 2

---
+++ ximesthrone/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "ximesthrone0f043b2b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- ximesthrone/src/App.js
+++ ximesthrone/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hellowwww :)
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          A01028604 Ximena Gonzalez
+          Learn React
         </a>
       </header>
     </div>
