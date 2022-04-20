7453770e15a0436ee301cfa8239df60b9e7023c6
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2022-04-20 16:42:29 -0500

    Added API

--- save-recordings/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ save-recordings/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "saverecordings": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
