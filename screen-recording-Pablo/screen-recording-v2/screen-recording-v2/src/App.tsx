989d165c789d3b6c7f52ec7977f4cb904dde672c
PabloYamamoto <A01022382@itesm.mx>
2022-04-21 14:27:16 -0500

    Starting V2 of the Screen Recorder

---
+++ screen-recording-Pablo/screen-recording-v2/screen-recording-v2/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h1>Hi</h1>
+  );
+}
+
+export default App;
---
+++ screen-recording-Pablo/screen-recording-v2/screen-recording-v2/src/UI/AppHeader/AppHeader.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const AppHeader = () => {
+  return (
+    <div>AppHeader</div>
+  )
+}
+
+
+export default AppHeader
