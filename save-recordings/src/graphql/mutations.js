6c8e067bab3e44d478e7516880ecee244a9c7016
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2022-04-21 16:14:54 -0500

    Added a new schema with their corresponding mutations, queries, and subscripcions

--- save-recordings/src/graphql/mutations.js
+++ save-recordings/src/graphql/mutations.js
@@ -1,6 +1,54 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const createVoiceRecordings = /* GraphQL */ `
+    mutation CreateVoiceRecordings(
+        $input: CreateVoiceRecordings!
+        $condition: ModelVoiceRecordingsConditionInput
+    ) {
+        createVoiceRecordings(input: $input, condition: $condition) {
+            id
+            agentId
+            voiceId
+            voicePath
+            createdAt
+            updatedAt
+        }
+    }
+`;
+
+export const updateVoiceRecordings = /* GraphQL */ `
+    mutation UpdateVoiceRecordings(
+        $input: UpdateVoiceRecordingsInput!
+        $condition: ModelVoiceRecordingsConditionInput
+    ) {
+      updateVoiceRecordings(input: $input, condition: $condition) {
+        id
+        agentId
+        voiceId
+        voicePath
+        createdAt
+        updatedAt
+      }
+    }
+`;
+
+export const deleteVoiceRecordings = /* GraphQL */ `
+  mutation DeleteVoiceRecordings(
+    $input: DeleteVoiceRecordingsInput!
+    $condition: ModelVoiceRecordingsConditionInput
+  ) {
+    deleteVoiceRecordings(input: $input, condition: $condition) {
+      id
+      agentId
+      voiceId
+      voicePath
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
 export const createVideoRecordings = /* GraphQL */ `
   mutation CreateVideoRecordings(
     $input: CreateVideoRecordingsInput!
--- save-recordings/src/graphql/subscriptions.js
+++ save-recordings/src/graphql/subscriptions.js
@@ -1,6 +1,39 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const getVoiceRecordings = /* GraphQL */ `
+    query GetVoiceRecordings($id: ID!) {
+        getVoiceRecordings(id: $id) {
+            id
+            agentId
+            voiceId
+            voicePath
+            createdAt
+            updatedAt
+        }
+    }
+`;
+
+export const listVoiceRecordings = /* GraphQL */ `
+    query ListVoiceRecordings(
+        $filter: ModelVoiceRecordingsFilterInput
+        $limit: Int
+        $nextToken: String
+    ) {
+        listVoiceRecordings(filter: $filter, limit: $limit, nextToken: $nextToken) {
+            items {
+                id
+                agentId
+                voiceId
+                voicePath
+                createdAt
+                updatedAt  
+            }
+            nextToken
+        }
+    }
+`;
+
 export const getVideoRecordings = /* GraphQL */ `
   query GetVideoRecordings($id: ID!) {
     getVideoRecordings(id: $id) {
