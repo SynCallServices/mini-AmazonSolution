48cb2300204b36533d2be9d94e76403af85064f8
SebastianJL <sebastianjuncos@gmail.com>
2022-04-22 16:29:14 -0500

    Uploading videos to Dynamo now works 🥳

--- no-borrar-app-juncos/src/App.js
+++ no-borrar-app-juncos/src/App.js
@@ -5,7 +5,7 @@ import '@aws-amplify/ui-react/styles.css';
 import awsconfig from './aws-exports';
 
 // GraphQl
-// import { createVideo, updateVideo, deleteVideo } from './graphql/mutations';
+import { createVideo, updateVideo, deleteVideo } from './graphql/mutations';
 import { listVideos } from './graphql/queries';
 
 // React 
@@ -16,7 +16,7 @@ Amplify.configure(awsconfig);
 
 function App({ signOut, user }) {
     // Empty video 
-    const video = { agentId: user.username, videoId: "-", videoPath: "/" };
+    const video = { agentId: user.username, videoId: "", videoPath: "" };
     // Use the empty video for the states
     // Idk what states are tho 🥴
     const [state, setState] = useState(video);
@@ -24,9 +24,7 @@ function App({ signOut, user }) {
 
     useEffect(() => { fetchVideos() }, []);
     // Figure wtf is this -> but later pa, con calmish
-    // const setInput = (key, value) => {
-    //     setState({...state, [key]:value})
-    //   }
+    const setInput = (key, value) => { setState({ ...state, [key]: value }); }
 
     async function fetchVideos() {
         try {
@@ -35,23 +33,62 @@ function App({ signOut, user }) {
             const videoList = videoData.data.listVideos.items;
             console.log('videos', videoList);
             setVideoRecordings(videoList);
-        } catch (error) { console.log('Error fetching Video recordings.', error.errors[0]); }
+        } catch (error) { console.log('Error fetching Videos 🥴', error.errors[0]); }
     }
+
+    async function addVideo() {
+        try {
+            if (!state.videoId || !state.videoPath) return;
+            const videoUpload = { ...state };
+            setVideoRecordings([...videoRecordings, videoUpload]);
+            setState(video);
+            // Create a video 
+            await API.graphql(graphqlOperation(createVideo, { input: videoUpload }));
+        } catch (error) { console.log('Error creating video 🥴 ', error); }
+    }
+
     // We'll use it later... I hope 
     // CRUD without the R -> CUD
-    //     /* create a todo */
-    //     await API.graphql(graphqlOperation(createVideo, { input: video }));
-    //     /* update a todo */
-    //     await API.graphql(graphqlOperation(updateVideo, { input: { id: user.username, videoPath: "Updated todo info" } }));
-    //     /* delete a todo */
-    //     await API.graphql(graphqlOperation(deleteVideo, { input: { id: user.username } }));
-
+    // Create a video 
+    // await API.graphql(graphqlOperation(createVideo, { input: video }));
+    // Update a todo
+    // await API.graphql(graphqlOperation(updateVideo, { input: { id: user.username, videoPath: "Updated todo info" } }));
+    // Delete a todo
+    // await API.graphql(graphqlOperation(deleteVideo, { input: { id: user.username } }));
 
     return (
-        <>
+        <div className='App'>
             <h1>Hello {user.username}</h1>
+
+            <h2>Your Videos</h2>
+            <input
+                onChange={event => setInput('videoId', event.target.value)}
+                value={state.videoId}
+                placeholder="Video Recording Id"
+            />
+            <input
+                onChange={event => setInput('videoPath', event.target.value)}
+                value={state.videoPath}
+                placeholder="Video Recording Path"
+            />
+            <button onClick={addVideo}>Create Video Recording entry</button>
+            {/* <button onClick={fetchVideos}>Get Video Recordings</button> */}
+
+            {
+                videoRecordings.map((video, index) => (
+                    <div key={video.id ? video.id : index} className='videoRecording'>
+                        <p className='videoRecordingVideoId'>Video Recording ID: {video.videoId}</p>
+                        <p className='videoRecordingVideoId'>Agent ID: {video.agentId}</p>
+                        <p className='videoRecordingVideoId'>Video Path: {video.videoPath}</p>
+                    </div>
+                ))
+            }
             <button onClick={signOut}>Sign out</button>
-        </>
+        </div>
+        // <>
+        //     <h1>Hello {user.username}</h1>
+        //     <button onClick={signOut}>Sign out</button>
+        // </>
 
     );
 }
