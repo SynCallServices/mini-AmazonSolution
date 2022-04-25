1bf9affcad6860e198af2ca9b3dbfb8e376e2a71
SebastianJL <sebastianjuncos@gmail.com>
2022-04-25 15:12:39 -0500

    Added S3 buckets and lambda

--- no-borrar-app-juncos/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ no-borrar-app-juncos/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,19 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "function": {
+        "S3Triggerf2fa0f4f": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "s3b8f538ba": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
