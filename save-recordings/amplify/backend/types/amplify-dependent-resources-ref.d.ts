1f267f5a19d3c5bd152da41d6332cebacee432f0
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2022-04-26 17:44:13 -0500

    Added Storage

--- save-recordings/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ save-recordings/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "s3Saverec": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
