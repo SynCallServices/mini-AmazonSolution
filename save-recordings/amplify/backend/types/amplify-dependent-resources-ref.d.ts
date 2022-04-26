0893855ffe3a22443e2c85b09918b0ccc1c6a54b
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2022-04-26 17:40:10 -0500

    Added auth to amplify project

--- save-recordings/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ save-recordings/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "saverecordings": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "saverecordingscfb29b09": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
