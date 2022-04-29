71a51fcc64ca638de76c49a4ee596c5ea3791ce5
SebastianJL <sebastianjuncos@gmail.com>
2022-04-29 16:36:16 -0500

    (wip) Moving App.js into modules, but it's not finished yet 😅

--- no-borrar-app-juncos/src/components/App.js
+++ no-borrar-app-juncos/src/components/App.js
@@ -1,22 +1,15 @@
 // AWS
-import { Amplify, API, graphqlOperation, Storage } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import awsconfig from './aws-exports';
-
-// GraphQl
-import { createVideo, updateVideo, deleteVideo } from './graphql/mutations';
-import { listVideos } from './graphql/queries';
+import uploadS3 from "../api/video";
+import videoAdd from "../api/video"
 
 // React 
 import { useEffect, useState, useRef } from 'react';
 
-// Necessary amplify configuration
-Amplify.configure(awsconfig);
-
 function App({ signOut, user }) {
     // Empty video 
-    const video = { agentId: user.username, videoId: "", videoPath: "" };
+    const video = { videoId: "", agentId: user.username, videoPath: "" };
     const [s3File, setS3File] = useState();
     const inputRef = useRef();
     // Use the empty video for the states
@@ -26,31 +19,11 @@ function App({ signOut, user }) {
     // This status will be used for a video player later in development
     // const [videoPlaying, setVideoPlaying] = useState('');
 
-    useEffect(() => { fetchVideos() }, []);
+    useEffect(() => { video }, []);
 
     // // Figure wtf is this -> but later pa, con calmish
-    const setInput = (key, value) => { setState({ ...state, [key]: value }); }
-
-    async function fetchVideos() {
-        try {
-            // Make read request from graphql -> DynamoDb
-            const videoData = await API.graphql(graphqlOperation(listVideos));
-            const videoList = videoData.data.listVideos.items;
-            console.log('videos', videoList);
-            setVideoRecordings(videoList);
-        } catch (error) { console.log('Error fetching Videos 🥴', error.errors[0]); }
-    }
-
-    async function addVideo() {
-        try {
-            if (!state.videoId || !state.videoPath) return;
-            const videoUpload = { ...state };
-            setVideoRecordings([...videoRecordings, videoUpload]);
-            setState(video);
-            // Create a video 
-            await API.graphql(graphqlOperation(createVideo, { input: videoUpload }));
-        } catch (error) { console.log('Error creating video 🥴 ', error); }
-        return window.location.reload();
+    const setInput = (key, value) => {
+        setState(prevState => ({ ...prevstate, [key]: value }));
     }
 
     // We'll use it later... I hope 
@@ -62,14 +35,6 @@ function App({ signOut, user }) {
     // Delete a todo
     // await API.graphql(graphqlOperation(deleteVideo, { input: { id: user.username } }));
 
-    async function uploadToS3(file) {
-        console.log(file);
-        try {
-            await Storage.put(file.name, file);
-            return true;
-        } catch (error) { console.log("Error uploading file: ", error); }
-        return false;
-    }
 
     return (
         <div className='App'>
@@ -92,8 +57,8 @@ function App({ signOut, user }) {
             />
             <br /><br />
             <button onClick={(event) => {
-                let uploadStatus = uploadToS3(s3File);
-                addVideo();
+                let uploadStatus = uploadS3(s3File);
+                videoAdd({ ...state })
                 let fileSection = document.getElementById('fileButton');
                 fileSection.value = '';
                 if (uploadStatus)
--- no-borrar-app-juncos/src/index.js
+++ no-borrar-app-juncos/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './assets/styles/index.css';
+import App from './components/App.js';
 import reportWebVitals from './reportWebVitals';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+// Necessary amplify configuration
+Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
