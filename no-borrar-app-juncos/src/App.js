78ec5604cfdb91e56b2420747c0cdc6fdf8e22ea
SebastianJL <sebastianjuncos@gmail.com>
2022-05-04 18:30:10 -0500

    Went back to 1 file, but now everything works

--- no-borrar-app-juncos/src/App.js
+++ no-borrar-app-juncos/src/App.js
@@ -1,29 +1,61 @@
 // AWS
+import { Amplify, API, graphqlOperation, Storage } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import uploadS3 from "../api/video";
-import videoAdd from "../api/video"
+import awsconfig from './aws-exports';
+
+// GraphQl
+import { createVideo, updateVideo, deleteVideo } from './graphql/mutations';
+import { listVideos } from './graphql/queries';
 
 // React 
 import { useEffect, useState, useRef } from 'react';
 
+// Necessary amplify configuration
+Amplify.configure(awsconfig);
+
 function App({ signOut, user }) {
     // Empty video 
-    const video = { videoId: "", agentId: user.username, videoPath: "" };
+    const video = { agentId: user.username, videoId: "", videoPath: "" };
     const [s3File, setS3File] = useState();
     const inputRef = useRef();
     // Use the empty video for the states
     // Idk what states are tho 🥴
     const [state, setState] = useState(video);
     const [videoRecordings, setVideoRecordings] = useState([]);
+    let file;
     // This status will be used for a video player later in development
     // const [videoPlaying, setVideoPlaying] = useState('');
 
-    useEffect(() => { video }, []);
+    useEffect(() => { fetchVideos() }, []);
 
-    // // Figure wtf is this -> but later pa, con calmish
-    const setInput = (key, value) => {
-        setState(prevState => ({ ...prevstate, [key]: value }));
+    const setInput = (key, value) => { setState({ ...state, [key]: value }); }
+
+    async function fetchVideos() {
+        try {
+            // Make read request from graphql -> DynamoDb
+            const videoData = await API.graphql(graphqlOperation(listVideos));
+            const videoList = videoData.data.listVideos.items;
+            console.log('videos', videoList);
+            setVideoRecordings(videoList);
+        } catch (error) { console.log('Error fetching Videos 🥴', error.errors[0]); }
+    }
+
+    async function fileUploaded(e) {
+        file = e.target.files[0];
+    }
+
+    async function addVideo() {
+        try {
+            if (!state.videoId || !state.videoPath) return;
+            const videoUpload = { ...state };
+            setVideoRecordings([...videoRecordings, videoUpload]);
+            setState(video);
+            await Storage.put(file.name, file)
+            // Create a video 
+            await API.graphql(graphqlOperation(createVideo, { input: videoUpload }));
+        } catch (error) { console.log('Error uploading video 🥴 ', error); }
+        return window.location.reload();
     }
 
     // We'll use it later... I hope 
@@ -52,18 +84,9 @@ function App({ signOut, user }) {
                 placeholder="Video Recording Path"
             />
             <br />
-            <input id='fileButton' ref={inputRef} type="file"
-                onChange={(event) => { setS3File(event.target.files[0]); }}
-            />
+            <input type="file" onChange={fileUploaded} />
             <br /><br />
-            <button onClick={(event) => {
-                let uploadStatus = uploadS3(s3File);
-                videoAdd({ ...state })
-                let fileSection = document.getElementById('fileButton');
-                fileSection.value = '';
-                if (uploadStatus)
-                    alert('Video has been uploaded successfully');
-            }}>Create Video Recording entry</button>
+            <button onClick={addVideo}>Create Video Recording entry</button>
 
             <ul>
                 {
